cb6970201e2b938a30cbd149f3e03c4ab5322835
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-09-18 08:20:43 +1000

    Basic styles include fonts, layouts, etc.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = `https://cdn.fonts.net/kit/${import.meta.env.VITE_MONOTYPE_CSS_TOKEN}.css`
+    link.crossOrigin = 'anonymous'
+
+    document.head.appendChild(link)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="w-full mx-4 lg:w-2/3 lg:mx-auto">
+    <div class="my-6">
+      <div class="text-4xl font-bold">Pseudo Password Generator</div>
+      <div class="text-2xl">Remastered Version</div>
+    </div>
+    <hr />
+  </section>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
