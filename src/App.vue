9b9cc7a8ecf8a5413ca03e38e05d74601e12cd0a
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-09-18 10:25:42 +1000

    Notes

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,20 @@ onUnmounted(() => {
       <div class="text-4xl font-bold">{{ $t('message.appname') }}</div>
       <div class="text-2xl">{{ $t('message.appname_second') }}</div>
     </div>
+
     <hr class="my-8" />
+
     <Generator />
+
+    <hr class="my-8" />
+    
+    <div>
+      <div class="text-3xl uppercase font-bold mb-4">{{$t("message.notes_title")}}</div>
+      <div class="flex flex-col gap-4" v-html="$t('message.notes_body')" />
+    </div>
+
+    <hr class="my-8" />
+
+    <div class="mb-4 text-sm text-slate-500 dark:text-slate-400">Made by <a href="https://astrian.moe" target="_blank" class="text-slate-500 dark:text-slate-400 underline">Astrian</a></div>
   </section>
 </template>
